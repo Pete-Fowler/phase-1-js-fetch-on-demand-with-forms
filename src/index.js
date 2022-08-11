1ab1b6f02fdb44d601860de6db912b907015bf92
Pete Fowler <fowler.pete@gmail.com>
2022-08-11 16:25:47 -0600

    commit tree 6b09d031d49aef0786c097bd1acbced711ff5a92

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
+  const summary = document.querySelector('p');
+  const title = document.querySelector('h4');
+
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let value = e.target.searchByID.value;
+    
+
+    fetch(`http://localhost:3000/movies/${value}`)
+    .then(res => res.json())
+    .then(data => {
+      title.textContent = data.title;
+      summary.textContent = data.summary;
+    });
+  }); 
+
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
